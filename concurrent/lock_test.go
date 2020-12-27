1df95dd4a0038003e7ec29da04fa01fbbd68dffd
Naorem Khogendro Singh <nkhogen@Naorems-MacBook-Pro.local>
2020-12-26 16:07:52 -0800

    Test fix

--- concurrent/lock_test.go
+++ concurrent/lock_test.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"testing"
 	"time"
+	"sync"
 )
 
 // Rudimentary test
 func TestNewCondLock(t *testing.T) {
 	cond := NewCondLock()
 	start := time.Now()
-	fmt.Printf("Waiting ...\n")
+	fmt.Printf("Waiting for 5 secs in main goroutine...\n")
 	cond.Lock()
 	cond.TimedWait(time.Second * 5)
-	fmt.Printf("Done ... %+v\n", time.Since(start)/time.Second)
+	fmt.Printf("Done waiting 5 secs in main goroutine... %+v\n", time.Since(start)/time.Second)
 	go func() {
-		t := time.Tick(time.Second * 5)
+		t := time.Tick(time.Second * 3)
 		t1 := time.Tick(time.Second * 10)
 		for {
 			select {
@@ -25,6 +26,7 @@ func TestNewCondLock(t *testing.T) {
 					cond.Lock()
 					defer cond.Unlock()
 					cond.Notify()
+					fmt.Printf("Notified...\n")
 				}()
 
 			case <-t1:
@@ -33,23 +35,26 @@ func TestNewCondLock(t *testing.T) {
 					cond.Lock()
 					defer cond.Unlock()
 					cond.NotifyAll()
+					fmt.Printf("Notified all...\n")
 				}()
 			}
 		}
 	}()
-
+	fmt.Printf("Unlocking in main goroutine so that other goroutines can lock to wait")
+	cond.Unlock()
+	wg := &sync.WaitGroup{}
+	wg.Add(5)
 	for i := 0; i < 5; i++ {
 		fmt.Printf("Waiting again %d ...\n", i)
-		go func() {
+		go func(n int) {
 			cond.Lock()
 			defer cond.Unlock()
 			cond.Wait()
-		}()
+			fmt.Printf("Done %d... %+v\n", n, time.Since(start)/time.Second)
+			wg.Done()
+		}(i)
 	}
-	cond.Wait()
 	fmt.Printf("Done ... %+v\n", time.Since(start)/time.Second)
-	fmt.Printf("Waiting again ...\n")
-	cond.TimedWait(time.Second * 5)
-	fmt.Printf("Done ... %+v\n", time.Since(start)/time.Second)
-	cond.Unlock()
+	wg.Wait()
+	fmt.Printf("Done all ... %+v\n", time.Since(start)/time.Second)
 }
